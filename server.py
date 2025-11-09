7226388be9ef979fb238228bc9e742c9f01f7431
Marco <boehme.m23@gmail.com>
2025-11-09 09:24:53 +0000

    11.9.25

--- server.py
+++ server.py
@@ -36,51 +36,6 @@ app.config["BEARER_TOKEN_MOVIE"] = os.environ.get("TMDB_BEARER_TOKEN")
 app.config["UPLOAD_FOLDER"] = "uploads"
 os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 BEARER_TOKEN_MOVIE = app.config.get("BEARER_TOKEN_MOVIE")
-SPOTIPY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
-SPOTIPY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
-SPOTIPY_REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI', 'https://website-0std.onrender.com/disney/callback')
-SCOPE = (
-    'user-read-private '
-    'user-read-email '
-    'user-read-playback-state '
-    'user-modify-playback-state '
-    'streaming '
-    'user-library-read '
-    'user-library-modify '
-    'playlist-read-private '
-    'playlist-read-collaborative '
-    'playlist-modify-public '
-    'playlist-modify-private '
-    'user-top-read '
-    'user-read-recently-played'
-)
-# Flask ensures 'session' is available when this part of the code is effectively used during a request.
-sp_oauth = SpotifyOAuth(
-    client_id=SPOTIPY_CLIENT_ID,
-    client_secret=SPOTIPY_CLIENT_SECRET,
-    redirect_uri=SPOTIPY_REDIRECT_URI,
-    scope=SCOPE,
-    cache_handler=FlaskSessionCacheHandler(session),  # Pass it directly
-    show_dialog=True
-)
-
-
-def get_spotify_for_user_():
-    # 'session' will be available because this function runs within a Flask request context.
-    user_cache_handler = FlaskSessionCacheHandler(session)
-    token_info = sp_oauth.validate_token(user_cache_handler.get_cached_token())
-    if not token_info:
-        # No valid token, redirect to login
-        return None, None
-
-    if sp_oauth.is_token_expired(token_info):
-        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
-        user_cache_handler.save_token_to_cache(token_info)  # Save refreshed token using this handler
-
-    sp = Spotify(auth_manager=sp_oauth)  # Use auth_manager with the cached token
-    return sp, token_info['access_token']
-
-
 Bootstrap(app)
 
 
